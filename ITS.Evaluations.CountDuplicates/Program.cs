0f20adc695a581d47db7b62cec0327e1c0f2e3d3
Diego Gabriel León Peña <diegabo1996@yahoo.es>
2022-04-04 23:06:19 -0400

    Create repository.

--- ITS.Evaluations.CountDuplicates/Program.cs
+++ ITS.Evaluations.CountDuplicates/Program.cs
@@ -9,29 +9,29 @@ namespace ITS.Evaluations.CountDuplicates
             int[] arr1 = new int[100];
             int[] arr2 = new int[100];
             int[] arr3 = new int[100];
-            int s1, mm = 1, ctr = 0;
+            int s, mm = 1, ctr = 0;
             int i, j;
             Console.Write("\n\nCount total number of duplicate items in an array:\n");
 
             Console.Write("Input the number of items to be stored in the array :");
-            s1 = Convert.ToInt32(Console.ReadLine());
+            s = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Input {0} items in the array :\n", s1);
-            for (i = 0; i < s1; i++)
+            Console.Write("Input {0} items in the array :\n", s);
+            for (i = 0; i < s; i++)
             {
                 Console.Write("item - {0} : ", i);
                 arr1[i] = Convert.ToInt32(Console.ReadLine());
             }
 
-            for (i = 0; i < s1; i++)
+            for (i = 0; i < s; i++)
             {
                 arr2[i] = arr1[i];
                 arr3[i] = 0;
             }
 
-            for (i = 0; i < s1; i++)
+            for (i = 0; i < s; i++)
             {
-                for (j = 0; j < s1; j++)
+                for (j = 0; j < s; j++)
                 {
                     if (arr1[i] == arr2[j])
                     {
@@ -41,7 +41,7 @@ namespace ITS.Evaluations.CountDuplicates
                 }
                 mm = 1;
             }
-            for (i = 0; i < s1; i++)
+            for (i = 0; i < s; i++)
             {
                 if (arr3[i] == 2) { ctr++; }
             }
